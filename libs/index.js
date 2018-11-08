87d302bc4cde6b3a16dc604968d6057aae24fd41
hideokamoto <hide.okamoto@digitalcube.jp>
2018-11-08 22:42:08 +0900

    init

---
+++ libs/index.js
@@ -0,0 +1,47 @@
+/**
+ * 文語調を口語調に変換する
+ * @param {string} desc content
+ */
+const replaceDescription = desc => {
+  let content = desc
+  if (/である。/g.test(desc)) content = content.replace(/である。/g, 'です。')
+  if (/する。/g.test(desc)) content = content.replace(/する。/g, 'します。')
+  if (/した。/g.test(desc)) content = content.replace(/した。/g, 'しました。')
+  if (/ないが、/g.test(desc)) content = content.replace(/ないが、/g, 'ありませんが、')
+  if (/たが、/g.test(desc)) content = content.replace(/たが、/g, 'ましたが、')
+  if (/いる。/g.test(desc)) content = content.replace(/いる。/g, 'います。')
+  if (/いた。/g.test(desc)) content = content.replace(/いた。/g, 'いました。')
+  return content
+}
+
+/**
+ * Alexaで発話できない記号を変換する
+ * @param {string} desc content for Alexa SSML
+ */
+const replaceSpeechContent = desc => {
+  let content = desc
+  if (/『/g.test(desc)) content = content.replace(/『/g, '')
+  if (/』/g.test(desc)) content = content.replace(/』/g, '')
+  if (/&/g.test(desc)) content = content.replace(/&/g, 'アンド')
+  if (/ - /g.test(desc)) content = content.replace(/ - /g, '生まれ')
+  return content
+}
+
+/**
+ * ラベル表記を修正したい時用
+ * @param {string} desc content
+ */
+const replaceLabel = desc => {
+  let content = desc
+  content = content.replace(/http:\/\/ja.dbpedia.org\/resource\//g, '')
+  if (/『/g.test(desc)) content = content.replace(/『/g, '「')
+  if (/』/g.test(desc)) content = content.replace(/』/g, '」')
+  if (/&/g.test(desc)) content = content.replace(/&/g, '＆')
+  return content
+}
+
+module.exports = {
+  replaceLabel,
+  replaceSpeechContent,
+  replaceDescription
+}
