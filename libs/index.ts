9cbfac535f220cb9c9df31175efaef422d65a36b
hideokamoto <hide.okamoto@digitalcube.jp>
2019-07-05 22:00:05 +0900

    fix

--- libs/index.ts
+++ libs/index.ts
@@ -3,15 +3,15 @@
  * @param {string} desc content
  */
 export const replaceDescription = (desc: string): string => {
-  let content = desc
-  if (/である。/g.test(desc)) content = content.replace(/である。/g, 'です。')
-  if (/する。/g.test(desc)) content = content.replace(/する。/g, 'します。')
-  if (/した。/g.test(desc)) content = content.replace(/した。/g, 'しました。')
-  if (/ないが、/g.test(desc)) content = content.replace(/ないが、/g, 'ありませんが、')
-  if (/たが、/g.test(desc)) content = content.replace(/たが、/g, 'ましたが、')
-  if (/いる。/g.test(desc)) content = content.replace(/いる。/g, 'います。')
-  if (/いた。/g.test(desc)) content = content.replace(/いた。/g, 'いました。')
-  return content
+    let content = desc
+    if (/である。/g.test(desc)) content = content.replace(/である。/g, 'です。')
+    if (/する。/g.test(desc)) content = content.replace(/する。/g, 'します。')
+    if (/した。/g.test(desc)) content = content.replace(/した。/g, 'しました。')
+    if (/ないが、/g.test(desc)) content = content.replace(/ないが、/g, 'ありませんが、')
+    if (/たが、/g.test(desc)) content = content.replace(/たが、/g, 'ましたが、')
+    if (/いる。/g.test(desc)) content = content.replace(/いる。/g, 'います。')
+    if (/いた。/g.test(desc)) content = content.replace(/いた。/g, 'いました。')
+    return content
 }
 
 /**
@@ -19,12 +19,12 @@ export const replaceDescription = (desc: string): string => {
  * @param {string} desc content for Alexa SSML
  */
 export const replaceSpeechContent = (desc: string): string => {
-  let content = desc
-  if (/『/g.test(desc)) content = content.replace(/『/g, '')
-  if (/』/g.test(desc)) content = content.replace(/』/g, '')
-  if (/&/g.test(desc)) content = content.replace(/&/g, 'アンド')
-  if (/ - /g.test(desc)) content = content.replace(/ - /g, '生まれ')
-  return content
+    let content = desc
+    if (/『/g.test(desc)) content = content.replace(/『/g, '')
+    if (/』/g.test(desc)) content = content.replace(/』/g, '')
+    if (/&/g.test(desc)) content = content.replace(/&/g, 'アンド')
+    if (/ - /g.test(desc)) content = content.replace(/ - /g, '生まれ')
+    return content
 }
 
 /**
@@ -32,10 +32,10 @@ export const replaceSpeechContent = (desc: string): string => {
  * @param {string} desc content
  */
 export const replaceLabel = (desc: string): string => {
-  let content = desc
-  content = content.replace(/http:\/\/ja.dbpedia.org\/resource\//g, '')
-  if (/『/g.test(desc)) content = content.replace(/『/g, '「')
-  if (/』/g.test(desc)) content = content.replace(/』/g, '」')
-  if (/&/g.test(desc)) content = content.replace(/&/g, '＆')
-  return content
+    let content = desc
+    content = content.replace(/http:\/\/ja.dbpedia.org\/resource\//g, '')
+    if (/『/g.test(desc)) content = content.replace(/『/g, '「')
+    if (/』/g.test(desc)) content = content.replace(/』/g, '」')
+    if (/&/g.test(desc)) content = content.replace(/&/g, '＆')
+    return content
 }
